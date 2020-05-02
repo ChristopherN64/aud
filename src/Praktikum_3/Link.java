07eabbfa96be3ae84969a0e80dd0b53215d8ec59
Christopher <ch-nieder@web.de>
2020-05-02 19:35:12 +0200

    Lösen von Praktikum 3

---
+++ src/Praktikum_3/Link.java
@@ -0,0 +1,21 @@
+package Praktikum_3;
+
+
+public class Link<T>
+{
+	public T daten;
+	public Link<T> naechster;
+
+	public Link(T daten, Link<T> naechster)
+	{
+		assert(daten != null);
+
+		this.daten = daten;
+		this.naechster = naechster;
+	}
+
+	public String toString()
+	{
+		return daten.toString();
+	}
+}
---
+++ src/Praktikum_3/ListeTest.java
@@ -0,0 +1,9 @@
+package Praktikum_3;
+
+public class ListeTest
+{
+	public static void main(String[] args)
+	{
+		new ListeGUI().setVisible(true);
+	}
+}
